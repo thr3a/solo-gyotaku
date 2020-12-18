3e30b21ae97869159714a16fc91f368ec4cd7c77
thr3a <a@example.com>
2020-12-19 05:51:11 +0900

    add: キャプチャ機能仮実装

---
+++ cli.js
@@ -0,0 +1,30 @@
+const commandLineArgs = require('command-line-args');
+const Capture = require('./lib/capture');
+
+process.on('unhandledRejection', (error) => {
+  console.log(cap.response('ng'));
+  console.error(error);
+  process.exit(1);
+});
+
+const optionDefinitions = [
+  {
+    name: 'url',
+    type: String
+  },
+  {
+    name: 'device',
+    type: String,
+    defaultValue: 'pc'
+  },
+];
+const options = commandLineArgs(optionDefinitions);
+
+const cap = new Capture(options);
+(async () => {
+  await cap.openBrowser();
+  await cap.goto();
+  await cap.save();
+  await cap.close();
+  console.log(cap.response('ok'));
+})();
