a93bb3e1c94ded6bd970045b23cea79af978bb78
thr3a <a@example.com>
2020-12-20 05:12:31 +0900

    update: エラー周り改善

--- routes/capture/index.js
+++ routes/capture/index.js
@@ -4,17 +4,27 @@ const Capture = require('../../lib/capture')
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     const cap = new Capture(request.query)
-    await cap.openBrowser()
-    await cap.goto()
-    const pdfBuffer = await cap.save()
-    const response = await cap.response()
-    await cap.close()
-    reply.type('application/pdf')
-    reply.headers({
-      'Content-Disposition': 'inline',
-      'X-Cap-Url': encodeURIComponent(response.url),
-      'X-Cap-Title': encodeURIComponent(response.title)
-    })
-    return reply.send(pdfBuffer)
+    try {
+      await cap.openBrowser()
+      await cap.goto()
+      const pdfBuffer = await cap.save()
+      const response = await cap.response()
+      await cap.close()
+      reply
+        .type('application/pdf')
+        .headers({
+          'Content-Disposition': 'inline',
+          'X-Cap-Url': encodeURIComponent(response.url),
+          'X-Cap-Title': encodeURIComponent(response.title)
+        })
+        .send(pdfBuffer)
+    } catch (error) {
+      await cap.close()
+      throw {
+        statusCode: 500,
+        message: error.message,
+        backtrace: error.stack.split('\n')
+      }
+    }
   })
 }
